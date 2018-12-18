7b858700e47b2bfebbf68201cf497f6e0d64f6cf
D. Fan <fandideyang@hotmail.com>
2018-12-18 10:57:44 +0800

    Updates

---
+++ precache-manifest.bf7b0dfbbcac52a54b29b46d243a191f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b4f9cebaf235013379c",
+    "url": "/RandShift/static/css/main.6fc9450c.chunk.css"
+  },
+  {
+    "revision": "0b4f9cebaf235013379c",
+    "url": "/RandShift/static/js/main.0b4f9ceb.chunk.js"
+  },
+  {
+    "revision": "a9aa9a14640e4f35c116",
+    "url": "/RandShift/static/js/1.a9aa9a14.chunk.js"
+  },
+  {
+    "revision": "39cc8b95720861113f2b",
+    "url": "/RandShift/static/js/runtime~main.39cc8b95.js"
+  },
+  {
+    "revision": "b163cf11203e6b30c9d0709c5955fe2a",
+    "url": "/RandShift/index.html"
+  }
+];
